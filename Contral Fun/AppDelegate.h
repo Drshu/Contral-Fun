63e5dc216912a7051029c4f524cc62e4a0f6983a
Shucheng <741932183@qq.com>
2016-02-22 16:17:25 +0800

    Initial Commit

---
+++ Contral Fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contral Fun
+//
+//  Created by shucheng on 16/2/22.
+//  Copyright © 2016年 shucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Contral Fun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contral Fun
+//
+//  Created by shucheng on 16/2/22.
+//  Copyright © 2016年 shucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
