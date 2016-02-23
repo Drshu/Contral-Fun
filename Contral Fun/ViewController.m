48e73d550aba3dd715d2751eb76b21399ccc8a27
Shucheng <741932183@qq.com>
2016-02-24 00:40:26 +0800

    Add SegmentedControl and switch
    添加了分段控件和开关，可以使用分段控件在开关和按钮之间进行切换。

--- Contral Fun/ViewController.m
+++ Contral Fun/ViewController.m
@@ -11,6 +11,10 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
 
 @end
 
@@ -19,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.sliderLabel.text=@"50";//可以保证在触摸滑动条之前也有一个正确的数据
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +38,28 @@
 -(IBAction)backgroundTap:(id)sender{
     [self.nameField resignFirstResponder];
     [self.numberField resignFirstResponder];
+}
+- (IBAction)sliderChanged:(UISlider *)sender {
+    int progress = lroundf(sender.value);//获取滑动条当前值
+    self.sliderLabel.text=[NSString stringWithFormat:@"%d",progress];
+}
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting=sender.isOn;//获取isOn属性值，这里
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+    if (sender.selectedSegmentIndex==0) {
+        self.leftSwitch.hidden= NO;
+        self.rightSwitch.hidden= NO;
+        self.doSomethingButton.hidden = YES;
+    }
+    else{
+        self.leftSwitch.hidden = YES;
+        self.rightSwitch.hidden = YES;
+        self.doSomethingButton.hidden = NO;
+    }
+}
+- (IBAction)buttonPressed:(id)sender {
 }
 @end
